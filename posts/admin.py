15b908ac781f545a5f334240a495d79cef40d114
bakyt-s <b.seiitov@gmail.com>
2023-05-19 15:54:25 +0300

    hw3

--- posts/admin.py
+++ posts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from posts.models import Product
+from posts.models import Product, Comment
 
 admin.site.register(Product)
+admin.site.register(Comment)
--- posts/views.py
+++ posts/views.py
@@ -10,9 +10,18 @@ def main_view(request):
 def products_view(request):
     if request.method == 'GET':
         products = Product.objects.all()
-
         context = {
             'products': products
         }
-
         return render(request, 'products/products.html', context=context)
+
+
+def product_detail_view(request, id_):
+    if request.method == 'GET':
+        product = Product.objects.get(id=id_)
+
+        context = {
+            'product': product,
+            'comments': product.comment_set.all()
+        }
+        return render(request, 'products/detail.html', context=context)
